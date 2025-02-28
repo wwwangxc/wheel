89b8334118eb42a42c5278b1b017970677b42488
Star Wang <w.xingchen@outlook.com>
2025-02-28 09:58:03 +0800

    feat(errorx): remove custom errors & add example (#7)

--- errorx/code.go
+++ errorx/code.go
@@ -1,11 +1,6 @@
 package errorx
 
+// ErrCode error code
 type ErrCode uint32
 
 const codeUnknown ErrCode = 0
-
-const (
-	codeServerError      ErrCode = 500_00
-	codeInvalidRequest   ErrCode = 500_01
-	codeInvalidArguments ErrCode = 500_02
-)
