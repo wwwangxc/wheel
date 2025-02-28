230c2b532a79ea332bd77519c3784509f7540443
wwwangxc <w.xingchen@outlook.com>
2025-02-28 09:19:50 +0800

    feat(coroutine): WithWaitGroupX

--- coroutine/coroutine.go
+++ coroutine/coroutine.go
@@ -9,10 +9,13 @@ import (
 func Go(fn func(), opts ...Option) {
 	o := newOptions(opts...)
 	wheel.DoIfNotNil(o.wg, func() { o.wg.Add(1) })
+	wheel.DoIfNotNil(o.wgx, func() { o.wgx.Add(1) })
 
 	go func() {
 		defer func() {
 			wheel.DoIfNotNil(o.wg, func() { o.wg.Done() })
+			wheel.DoIfNotNil(o.wgx, func() { o.wgx.Done() })
+
 			if err := recover(); err != nil {
 				wheel.DoIfNotNil(o.logFn, func() { o.logFn(err) })
 			}
--- coroutine/coroutine_test.go
+++ coroutine/coroutine_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 
 	"github.com/wwwangxc/wheel/coroutine"
+	"github.com/wwwangxc/wheel/syncx"
 )
 
 func TestGo(t *testing.T) {
@@ -31,6 +32,22 @@ func TestGo(t *testing.T) {
 			})
 
 		})
+
+		Convey("Go with syncx.WaitGroup", func() {
+			var wg syncx.WaitGroup
+			coroutine.Go(fn, coroutine.WithWaitGroupX(&wg))
+			coroutine.Go(fn, coroutine.WithWaitGroupX(&wg))
+
+			Convey("Wait group should be waitting", func() {
+				So(&wg, shouldBeWaitting)
+			})
+
+			Convey("Wait group should not be waitting", func() {
+				time.Sleep(200 * time.Millisecond)
+				So(&wg, shouldNotBeWaitting)
+			})
+
+		})
 	})
 }
 
@@ -40,10 +57,12 @@ func shouldBeWaitting(actual any, expected ...any) string {
 	switch g := actual.(type) {
 	case *sync.WaitGroup:
 		wait = func() { g.Wait() }
+	case *syncx.WaitGroup:
+		wait = func() { g.Wait() }
 	case coroutine.Group:
 		wait = func() { g.Wait() }
 	default:
-		return "The type of actual should be *sync.WaitGroup or coroutine.Group"
+		return "The type of actual should be in [*sync.WaitGroup|*syncx.WaitGroup|coroutine.Group]"
 	}
 
 	done := make(chan struct{})
@@ -67,10 +86,12 @@ func shouldNotBeWaitting(actual any, expected ...any) string {
 	switch g := actual.(type) {
 	case *sync.WaitGroup:
 		wait = func() { g.Wait() }
+	case *syncx.WaitGroup:
+		wait = func() { g.Wait() }
 	case coroutine.Group:
 		wait = func() { g.Wait() }
 	default:
-		return "The type of actual should be *sync.WaitGroup or coroutine.Group"
+		return "The type of actual should be in [*sync.WaitGroup|*syncx.WaitGroup|coroutine.Group]"
 	}
 
 	done := make(chan struct{})
